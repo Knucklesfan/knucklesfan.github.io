75aa44dd67ab2570fc48c8c742f9a44a0ad1558b
Knucklesfan <cademparker@gmail.com>
2025-02-23 19:32:34 -0600

    more website work

--- js/coverflow.js
+++ js/coverflow.js
@@ -7,13 +7,22 @@ function coverflow_main() {
     let hoveringLeftButton = false;
     let hoveringRightButton = false;
     let subTransition = 0;
-
+    let hoveringCenter = false;
+    let hoveringBackground = false;
+    let spinnytimer = 0;
     let coverflow_images = [
-        ["images/coverflow_images/knfn.png",4/3],
-        ["images/coverflow_images/reflections.png",1],
-        ["images/coverflow_images/inevitability.png",4/3],
-        ["images/coverflow_images/inevitability.png",4/3],
-        ["images/coverflow_images/inevitability.png",4/3],
+        ["images/coverflow_images/knfn.png",4/3,{"title":"Knuxfan's Tetriminos","images":["images/coverflow_items/sdltris/knfn.png","images/coverflow_items/sdltris/shot1.png","images/coverflow_items/sdltris/shot2.png","images/coverflow_items/sdltris/shot3.png"],
+            "desc":"Knuxfan's Tetriminos is my longest project and the hardest I've worked on anything.<br>Written in it's own entire game engine, I built the game from the ground up using only C++ and OpenGL3.1.<br>The game is compatible with 10 different operating systems and game consoles, with more in the works!<br><br>The game has been in development since 2021, and will be released before I die (Which means... it's coming eventually...)","programming-languages":"C++","link":"https://github.com/knucklesfan/sdltris"}],
+        ["images/coverflow_images/reflections.png",1,{"title":"Reflections","images":["images/coverflow_items/reflections/shot2.png","images/coverflow_items/reflections/shot3.png","images/coverflow_items/reflections/shot1.png"],
+            "desc":"Self-Reflection is a mental health counseling app, written for use by Middle and High schoolers to easily and quickly reach out to counselors at any time and schedule appointments.<br>Reflections was my first full-stack application and is available for download from the Apple App Store (Google Play is currently down, but will be back up shortly) and can also be accessed from the web.","programming-languages":"Kotlin, Swift, Dart (Flutter), PHP, Javascript (original backend), MySQL, DynamoDB","link":"https://apps.apple.com/us/app/self-reflection/id1636196489"}],
+        ["images/coverflow_images/imageshuffle.png",1,{"title":"ImageShuffle","images":["images/coverflow_items/imageshuffle/image1.gif","images/coverflow_items/imageshuffle/output.apng","images/coverflow_items/imageshuffle/horzsat50.png"],"desc":"ImageShuffle is a weird program that lets you sort the pixels in an image, alongside giving you the tools to generate images that can contain two or more images depending on the layering.<br><br> ImageShuffle came from the idea originally of making a program that could sort the pixels in an image by color, but then I got carried away and realized you could hide entire images in other images using this sorting method.<br><br>ImageShuffle allows you to hide images while preserving full color depth, or full height depth, and it just makes a cool animation.<br>","programming-languages":"Python","link":"https://github.com/knucklesfan/ImageShuffle"}],
+        ["images/coverflow_images/waiter1.png",1,
+        {"title":"KnuxfanWaiter","images":["images/coverflow_items/knuxfanwaiter/waiter1.png","images/coverflow_items/knuxfanwaiter/waiter2.png","images/coverflow_items/knuxfanwaiter/waiter3.png","images/coverflow_items/knuxfanwaiter/waiter4.png"],"desc":"Waiter is an interactive screensaver, designed to be used on Chromebook devices that double as servers. (specific use-case, I know.)<br>Why did I make this, exactly? Well, I have come into ownership of a ton of old, outdated chromebooks, and need some way to tie my several networks together to my one central server, located offsite. I have 3 houses that I manage for my family, and Wireguard links these houses together into one Home Assistant instance, and since a Chromebook is used, why not use that fancy screen and keyboard for something interesting?<br><br>Waiter currently provides a few different scenes/services, including a scrolling, on-screen news ticker, a weather scene, a time scene, and a bouncing DVD Logo.","programming-languages":"Python","link":"https://github.com/knucklesfan/KnuxfanWaiter"}],
+        ["images/coverflow_images/flicewater.png",1],
+        ["images/coverflow_images/dsonut.png",256/384],
+        ["images/coverflow_images/knuxfanspelling.png",32/25],
+        ["images/coverflow_images/skyy.png",4/3],
+
 
     ]    
     coverflow_textures = []
@@ -53,6 +62,10 @@ function coverflow_main() {
         mousey = mousePos.y;
         hoveringRightButton = mousex>canvas.width-128&&(mousey>(canvas.height/2)-32 && mousey<(canvas.height/2)+32);
         hoveringLeftButton = mousex<128&&(mousey>(canvas.height/2)-32 && mousey<(canvas.height/2)+32);
+        hoveringCenter = mousex<(canvas.width/2)+(canvas.width/6)&&mousex>(canvas.width/2)-(canvas.width/6) //i know the width is hard-coded, but who's gonna notice that
+        hoveringBackground = mousex<(canvas.width/4)+192&&mousex>(canvas.width/4)-192 || mousex<(canvas.width-(canvas.width/4))+192&&mousex>(canvas.width-(canvas.width/4))-192
+        console.log(hoveringBackground)
+        console.log(hoveringCenter)
     }, false);
 
     mouselayer.addEventListener("mousedown",function (evt) {
@@ -70,6 +83,29 @@ function coverflow_main() {
             }
 
         }
+        if(hoveringCenter) { //load the actual thing
+            spinnytimer+=360;
+            let slideshowholder = document.getElementById("slideshow-location")
+            document.getElementById("modaltitle").innerHTML = coverflow_images[selection][2]["title"]
+            document.getElementById("modaldesc").innerHTML = coverflow_images[selection][2]["desc"]
+            slideshowholder.innerHTML = ""
+            for(let i = 0; i < coverflow_images[selection][2]["images"].length; i++) {
+                coverflow_images[selection][2]["images"][i]
+                slideshowholder.innerHTML += `<div class="mySlides fade" style="text-align: center;">
+                <div class="numbertext">${i+1} / ${coverflow_images[selection][2]["images"].length}</div>
+                <img src="${coverflow_images[selection][2]["images"][i]}" style="max-height:50vh;width:100%;  object-fit: contain;">
+                </div>
+                `
+            }
+            slideshowholder.innerHTML += `<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            `;
+            resetSlides(coverflow_images[selection][2]["images"].length)
+            document.getElementById("projectModal").style.display = "block";
+        }
+        if(hoveringBackground) { //little secret easter egg if you click on a background icon
+            spinnytimer+=360;
+        }
         if(hoveringRightButton) {
             if(selection+1 >= coverflow_images.length) {
                 subTransition += -coverflow_textures.length;
@@ -85,7 +121,12 @@ function coverflow_main() {
     }, false)
         // Draw the scene repeatedly
     function render(now) {
-        
+        if(spinnytimer > 0.1) {
+            spinnytimer = lerp(spinnytimer,0,0.01)
+        }
+        else {
+            spinnytimer = 0;
+        }
         if (subTransition != 0) {
             let lerpval = lerp(subTransition,0,0.05);
             if(Math.abs(lerpval) < 0.0001) {
@@ -95,7 +136,6 @@ function coverflow_main() {
                 subTransition = lerpval;
             }
         }
-        console.log(hoveringRightButton)
         if(hoveringLeftButton && buttonLeftSize < 1.2) {
             buttonLeftSize = lerp(buttonLeftSize,1.2,0.05)
             if(buttonLeftSize > 1.199) {
@@ -122,7 +162,7 @@ function coverflow_main() {
 
         }
 
-        if(hoveringLeftButton || hoveringRightButton) {
+        if(hoveringLeftButton || hoveringRightButton || hoveringCenter) {
             mouselayer.style.cursor = "pointer"
         }
         else {
@@ -141,17 +181,15 @@ function coverflow_main() {
             let equation = i+selection*-1+subTransition
             let x = -8.0+(Math.cos(equation*rad(180/coverflow_textures.length))-1)*8+(i==selection)*1
             let z = Math.sin((equation*rad(180/coverflow_textures.length)))*8
-            if(i == 0) {
-                console.log(x,z)
-            }
+
             let width = coverflow_images[i][1]
             c.render(canvas,gl,coverflow_textures[i],[
                 z,groundHeight+1,
-                x],[0,90+(Math.sin((Date.now()-starttime)/1000)*4),0],[0.05,1,width],[0,4,-8],1.0,fov)
+                x],[0,90+(Math.sin((Date.now()-starttime)/1000)*4)+spinnytimer,0],[0.05,1,width],[0,4,-8],1.0,fov)
             c.render(canvas,gl,coverflow_textures[i],[
                 z,
                 groundHeight-1,
-                x],[0,90+(Math.sin((Date.now()-starttime)/1000)*4),180],[0.05,1,width],[0,4,-8],1.0,fov)
+                x],[0,90+(Math.sin((Date.now()-starttime)/1000)*4)+spinnytimer,180],[0.05,1,width],[0,4,-8],1.0,fov)
         }
         p.render(canvas,gl,0,0,floor_texture,[0,groundHeight,-16],[-90,0,0],[16,16,1],[0,4,-8],0.8,fov)
         s.render(canvas,gl,32,canvas.height/2-32,button_texture,64*buttonLeftSize,64*buttonLeftSize,0,0,-1,1)
@@ -162,6 +200,19 @@ function coverflow_main() {
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
+    var modal = document.getElementById("projectModal");
+
+    // Handle modal related stuff too, including clicking on the darkened background
+    window.onclick = function(event) {
+        console.log(event.target)
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    document.getElementById("modalclose").onclick = function() {
+        modal.style.display = "none";
+    }
 
 }
 window.addEventListener('load',coverflow_main,false);
@@ -170,3 +221,29 @@ window.addEventListener('resize',function(evt) {
     document.getElementById("coverflow-canvas").height = document.getElementById("coverflow-canvas").getBoundingClientRect().height
     },false
 )
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function resetSlides(n) {
+    slideIndex = 1;
+    showSlides(n);
+}
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
